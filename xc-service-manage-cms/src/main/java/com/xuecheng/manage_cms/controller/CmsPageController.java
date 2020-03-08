4469ad410f31fde3d9d719979a15205d9ba8dab4
suboshi <1262945983@qq.com>
2020-03-08 22:32:49 +0800

    Initial commit

---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/controller/CmsPageController.java
@@ -0,0 +1,66 @@
+package com.xuecheng.manage_cms.controller;
+
+import com.xuecheng.api.cms.CmsPageControllerApi;
+import com.xuecheng.framework.domain.cms.CmsPage;
+import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
+import com.xuecheng.framework.domain.cms.response.CmsPageResult;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+import com.xuecheng.manage_cms.service.PageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ * @create 2018-09-12 17:24
+ **/
+@RestController
+@RequestMapping("/cms/page")
+public class CmsPageController implements CmsPageControllerApi {
+
+    @Autowired
+    PageService pageService;
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Override
+    @GetMapping("/list/{page}/{size}")
+    public QueryResponseResult findList(@PathVariable("page") int page, @PathVariable("size")int size, QueryPageRequest queryPageRequest) {
+
+/*        //暂时用静态数据
+        //定义queryResult
+        QueryResult<CmsPage> queryResult =new QueryResult<>();
+        List<CmsPage> list = new ArrayList<>();
+        CmsPage cmsPage = new CmsPage();
+        cmsPage.setPageName("测试页面");
+        list.add(cmsPage);
+        queryResult.setList(list);
+        queryResult.setTotal(1);
+
+        QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS,queryResult);
+        return queryResponseResult;*/
+        //调用service
+        return pageService.findList(page,size,queryPageRequest);
+    }
+
+    //添加页面
+    @Override
+    @PostMapping("/add")
+    public CmsPageResult add(@RequestBody CmsPage cmsPage) {
+        return pageService.add(cmsPage);
+    }
+
+    @GetMapping("/banner")
+    public String index_banner(Map<String, Object> map){
+        String dataUrl = "http://localhost:31001/cms/config/getmodel/5a791725dd573c3574ee333f";
+        ResponseEntity<Map> entity = restTemplate.getForEntity(dataUrl, Map.class);
+        Map body = entity.getBody();
+        map.putAll(body);
+        return "index_banner";
+    }
+}
---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/service/PageService.java
@@ -0,0 +1,68 @@
+package com.xuecheng.manage_cms.service;
+
+import com.xuecheng.framework.domain.cms.CmsPage;
+import com.xuecheng.framework.domain.cms.request.QueryPageRequest;
+import com.xuecheng.framework.domain.cms.response.CmsPageResult;
+import com.xuecheng.framework.model.response.CommonCode;
+import com.xuecheng.framework.model.response.QueryResponseResult;
+import com.xuecheng.framework.model.response.QueryResult;
+import com.xuecheng.manage_cms.dao.CmsPageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Administrator
+ * @version 1.0
+ * @create 2018-09-12 18:32
+ **/
+@Service
+public class PageService {
+
+    @Autowired
+    CmsPageRepository cmsPageRepository;
+
+
+    /**
+     * 页面查询方法
+     * @param page 页码，从1开始记数
+     * @param size 每页记录数
+     * @param queryPageRequest 查询条件
+     * @return
+     */
+    public QueryResponseResult findList(int page, int size, QueryPageRequest queryPageRequest){
+
+        //分页参数
+        if(page <=0){
+            page = 1;
+        }
+        page = page -1;
+        if(size<=0){
+            size = 10;
+        }
+        Pageable pageable = PageRequest.of(page,size);
+        Page<CmsPage> all = cmsPageRepository.findAll(pageable);
+        QueryResult queryResult = new QueryResult();
+        queryResult.setList(all.getContent());//数据列表
+        queryResult.setTotal(all.getTotalElements());//数据总记录数
+        QueryResponseResult queryResponseResult = new QueryResponseResult(CommonCode.SUCCESS,queryResult);
+        return queryResponseResult;
+    }
+
+    //添加页面
+    public CmsPageResult add(CmsPage cmsPage){
+        //校验页面是否存在，根据页面名称、站点Id、页面webpath查询
+        CmsPage cmsPage1 = cmsPageRepository.findByPageNameAndSiteIdAndPageWebPath
+                (cmsPage.getPageName(), cmsPage.getSiteId(), cmsPage.getPageWebPath());
+        if(cmsPage1==null){
+            cmsPage.setPageId(null);
+        //添加页面主键由spring data 自动生成
+            cmsPageRepository.save(cmsPage);
+            //返回结果
+            CmsPageResult cmsPageResult = new CmsPageResult(CommonCode.SUCCESS,cmsPage);
+            return cmsPageResult;
+        }
+        return new CmsPageResult(CommonCode.FAIL,null); }
+}
